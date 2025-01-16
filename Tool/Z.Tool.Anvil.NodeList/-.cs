cd8ac0e5d29b3c3506b95f89b70ab2c1da200381
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-16 18:31:38 +0800

    Tool

--- Tool/Z.Tool.Anvil.NodeList/-.cs
+++ Tool/Z.Tool.Anvil.NodeList/-.cs
@@ -4,4 +4,5 @@ global using ClassRead = Z.Tool.NodeListGen.Read;
 global using ClassNodeGen = Z.Tool.NodeListGen.NodeGen;
 global using ClassNewStateGen = Z.Tool.NodeListGen.NewStateGen;
 global using ClassNodeStateGen = Z.Tool.NodeListGen.NodeStateGen;
+global using ClassCreateSetStateGen = Z.Tool.NodeListGen.CreateSetStateGen;
 global using STAThreadAttribute = System.STAThreadAttribute;
