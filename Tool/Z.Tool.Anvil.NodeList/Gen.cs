231d793de6a06a4504ed09df7e68c79179cf4c57
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-16 14:35:09 +0800

    Tool

--- Tool/Z.Tool.Anvil.NodeList/Gen.cs
+++ Tool/Z.Tool.Anvil.NodeList/Gen.cs
@@ -2,4 +2,11 @@ namespace Z.Tool.Anvil.NodeList;
 
 public class Gen : ClassGen
 {
+    protected override ClassRead CreateRead()
+    {
+        ClassRead a;
+        a = new Read();
+        a.Init();
+        return a;
+    }
 }
